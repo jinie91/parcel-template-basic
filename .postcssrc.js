7b4d40afe834344384da265d915e74a8858748f5
jinie91 <h.hyejin218@gmail.com>
2021-12-02 15:17:11 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,26 @@
+// ESM 
+
+// import autoprefixer from 'autoprefixer'
+// export {
+//   plugins: [
+  //     autoprefixer
+  //   ]
+  // }
+
+
+// Commomjs
+
+// const autoprefixer = require('autoprefixer')
+    
+// module.exports = {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+
+// 간소화    
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
